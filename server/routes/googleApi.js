10e1aed65e90cc1a1987e4b2192ea1788f740cf0
ChancellorKupersmith <kupersmith3.6@gmail.com>
2024-08-22 16:20:08 -0700

    added route for serving client google api key

--- server/routes/googleApi.js
+++ server/routes/googleApi.js
@@ -3,6 +3,12 @@ const fetch = require('node-fetch');
 
 const router = express.Router();
 
+router.get('client_api_key', async (req, res) => {
+    res.json({
+        apiKey: process.env.GOOGLE_MAPS_API_KEY_CLIENT
+    });
+});
+
 router.get('/map', async (req, res, next) => {
     console.log('Hit')
     try {
