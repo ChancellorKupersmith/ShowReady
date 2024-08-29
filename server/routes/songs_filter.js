0eec3a8f3868ebb487a7d2fe3e7d9c39862095ea
ChancellorKupersmith <kupersmith3.6@gmail.com>
2024-08-28 22:38:51 -0700

    refactored backend filter endpoint to handle new filter state structure

--- server/routes/songs_filter.js
+++ server/routes/songs_filter.js
@@ -7,44 +7,14 @@ const pool = new Pool({
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
 });
-/*
-  This is how were gonna do it: the client will recieve a cache of default songs on
-  loading page. Then the cache will be updated based on velocity of scroll and
-  change in filter params requests.
-
-  // params that are always needed are events date range 
-  // acting as the data's pager. Then certain where conditions
-  // are dynamically added based on filter list not empty (ex:
-  // exclude_artists = ['a', 'b'] and only_venue_names = [''])
-  
-  SELECT Artists.Name, *.Songs, *.Events FROM Songs
-  JOIN Artists ON Artists.ID = Songs.ArtistID
-
-  JOIN EventsArtists ON EventsArtists.ArtistID = Artists.ID
-  JOIN Events ON Events.ID = EventsArtists.EventID
-  WHERE Events.EventDate >= (MIN_DATE_RANGE) 
-  AND Events.EventDate <= (MAX_DATE_RANGE)
-  
-  // not these artists
-  AND Artists.Name NOT IN (:excluded_names)
-  // only these artists
-  AND Artists.Name IN (:only_names)
-  // by venue name
-  AND Events.Venue
-  // with in certain radius
-  AND (
-    <distance_calculation_logic_using_latitude_and_longitude> <= <radius_in_meters>
-  )  
-*/
- 
- const MAX_PAGE_SIZE = 200;
- const DEFAULT_PAGE_SIZE = 100;
- const OrderBys = Object.freeze({
+const MAX_PAGE_SIZE = 200;
+const DEFAULT_PAGE_SIZE = 100;
+const OrderBys = Object.freeze({
    ARTIST: 'a.Name',
    SONG_NAME: 'Songs.Name',
    EVENT_DATE: 'e.Date',
    VENUE_NAME: 'v.Name'
-  });
+});
 
 const reqQueryParamsCleaner = (req, res, next) => {
   try {
@@ -72,6 +42,30 @@ const reqQueryParamsCleaner = (req, res, next) => {
   }
 };
 
+/*
+  // params that are always needed are events date range 
+  // acting as the data's page size. Then certain where conditions
+  // are dynamically added based on filter list not empty (ex:
+  // exclude_artists = ['a', 'b'] and only_venue_names = [''])
+  
+  SELECT Artists.Name, *.Songs, *.Events FROM Songs
+  JOIN Artists ON Artists.ID = Songs.ArtistID
+  JOIN EventsArtists ON EventsArtists.ArtistID = Artists.ID
+  JOIN Events ON Events.ID = EventsArtists.EventID
+  WHERE Events.EventDate >= (MIN_DATE_RANGE) 
+  AND Events.EventDate <= (MAX_DATE_RANGE)
+  
+  // not these artists
+  AND Artists.Name NOT IN (:excluded_names)
+  // only these artists
+  AND Artists.Name IN (:only_names)
+  // by venue name
+  AND Events.Venue
+  // with in certain radius
+  AND (
+    <distance_calculation_logic_using_latitude_and_longitude> <= <radius_in_meters>
+  )  
+*/
 const querySongsList = async (whereConditional, queryParms) => {
   const client = await pool.connect();
   // query = `
@@ -115,20 +109,35 @@ const querySongsList = async (whereConditional, queryParms) => {
 const whereConditionBuilder = async (req, res, next) => {
   try {
     let whereConditional = 'WHERE ';
-
     const { filters } = req.body;
-    if(filters.ex.minDate != '' && filters.ex.maxDate != '') whereConditional += `e.EventDate NOT BETWEEN '${ (new Date(filters.ex.minDate)).toUTCString() }' AND '${ (new Date(filters.ex.maxDate)).toUTCString() }' AND `;
-    if(filters.req.minDate != '' && filters.req.maxDate != '') whereConditional += `e.EventDate BETWEEN '${ (new Date(filters.req.minDate)).toUTCString() }' AND '${ (new Date(filters.req.maxDate)).toUTCString() }' AND `;
-    if(filters.ex.dates.length) whereConditional += `e.EventDate NOT IN ('${filters.ex.dates.join(`', '`)}') AND `;
-    if(filters.req.dates.length) whereConditional += `e.EventDate IN ('${filters.req.dates.join(`', '`)}') AND `;
-    if(filters.ex.artists.length) whereConditional += `a.Name NOT IN ('${filters.ex.artists.join(`', '`)}') AND `;
-    if(filters.req.artists.length) whereConditional += `a.Name IN ('${filters.req.artists.join(`', '`)}') AND `;
-    if(filters.ex.venues.length) whereConditional += `v.Name NOT IN ('${filters.ex.venues.join(`', '`)}') AND `;
-    if(filters.req.venues.length) whereConditional += `v.Name IN ('${filters.req.venues.join(`', '`)}') AND `;
-    if(filters.ex.songs.length) whereConditional += `Songs.Name NOT IN ('${filters.ex.songs.join(`', '`)}') AND `;
-    if(filters.req.songs.length) whereConditional += `Songs.Name IN ('${filters.req.songs.join(`', '`)}') AND `;
-    if(filters.ex.hoods.length) whereConditional += `v.Hood NOT IN ('${filters.ex.hoods.join(`', '`)}') AND `;
-    if(filters.req.hoods.length) whereConditional += `v.Hood IN ('${filters.req.hoods.join(`', '`)}') AND `;
+    if(filters.dateGThan != '') whereConditional += `e.EventDate >= '${ (new Date(filters.dateGThan)).toUTCString() }' AND `;
+    if(filters.dateLThan != '') whereConditional += `e.EventDate <= '${ (new Date(filters.dateLThan)).toUTCString() }' AND `;
+    if(filters.priceGThan != '') whereConditional += `e.Price >= '${filters.priceGThan}' AND `;
+    if(filters.priceLThan != '') whereConditional += `e.Price <= '${filters.priceLThan}' AND `;
+    // TODO: handle spotifyPopularity LThan/GThan
+    if(filters.ex.date.dates.length) whereConditional += `e.EventDate NOT IN ('${filters.ex.date.dates.join(`', '`)}') AND `;
+    if(filters.req.date.dates.length) whereConditional += `e.EventDate IN ('${filters.req.date.dates.join(`', '`)}') AND `;
+    if(filters.ex.date.eventTimes.length) whereConditional += `e.EventTime NOT IN ('${filters.ex.date.eventTimes.join(`', '`)}') AND `;
+    if(filters.req.date.eventTimes.length) whereConditional += `e.EventTime IN ('${filters.req.date.eventTimes.join(`', '`)}') AND `;
+    if(filters.ex.location.venues.length) whereConditional += `v.Name NOT IN ('${filters.ex.location.venues.join(`', '`)}') AND `;
+    if(filters.req.location.venues.length) whereConditional += `v.Name IN ('${filters.req.location.venues.join(`', '`)}') AND `;
+    if(filters.ex.location.hoods.length) whereConditional += `v.Hood NOT IN ('${filters.ex.location.hoods.join(`', '`)}') AND `;
+    if(filters.req.location.hoods.length) whereConditional += `v.Hood IN ('${filters.req.location.hoods.join(`', '`)}') AND `;
+    // TODO: handle ex/req addresses within square miles
+    if(filters.ex.event.names.length) whereConditional += `e.Name NOT IN ('${filters.ex.event.names.join(`', '`)}') AND `;
+    if(filters.req.event.names.length) whereConditional += `e.Name IN ('${filters.req.event.names.join(`', '`)}') AND `;
+    // TODO: handle age restrictions
+    if(filters.ex.artist.names.length) whereConditional += `a.Name NOT IN ('${filters.ex.artist.names.join(`', '`)}') AND `;
+    if(filters.req.artist.names.length) whereConditional += `a.Name IN ('${filters.req.artist.names.join(`', '`)}') AND `;
+    // TODO: handle from each artists
+    // TODO: handle from artist musicbrainz meta
+    if(filters.ex.album.names.length) whereConditional += `al.Name NOT IN ('${filters.ex.album.names.join(`', '`)}') AND `;
+    if(filters.req.album.names.length) whereConditional += `al.Name IN ('${filters.req.album.names.join(`', '`)}') AND `;
+    // TODO: handle from each album
+    if(filters.ex.song.names.length) whereConditional += `Songs.Name NOT IN ('${filters.ex.song.names.join(`', '`)}') AND `;
+    if(filters.req.song.names.length) whereConditional += `Songs.Name IN ('${filters.req.song.names.join(`', '`)}') AND `;
+    if(filters.ex.source.spotify) whereConditional += `Songs.SpotifyExternalId IS NULL AND `;
+    if(filters.req.source.spotify) whereConditional += `Songs.SpotifyExternalId IS NOT NULL AND `;
     // remove trailing 'AND'
     whereConditional = whereConditional.substring(0, whereConditional.length - 4);
     req.whereConditional = whereConditional;
@@ -139,13 +148,13 @@ const whereConditionBuilder = async (req, res, next) => {
 };
 
 
+// ROUTES
 const router = express.Router();
-// Optimize TODO (potential): SETUP CACHE OF songs list to avoid many sql requests
+// (potential) Optimize TODO: setup cache of songs list to avoid many sql requests
 router.post('/', reqQueryParamsCleaner, whereConditionBuilder, async (req, res, next) => {
   try {
-    const result = await querySongsList(req.whereConditional ,req.cParams);
+    const result = await querySongsList(req.whereConditional, req.cParams);
     const songsList = result.rows;
-    // console.log(songsList)
     res.json(songsList);
   } catch (err) {
     next(err);
