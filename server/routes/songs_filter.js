55546713669eaf862bc8c7c6a1357d0218792211
ChancellorKupersmith <kupersmith3.6@gmail.com>
2025-02-14 12:12:49 -0800

    added password for db connections

--- server/routes/songs_filter.js
+++ server/routes/songs_filter.js
@@ -6,6 +6,7 @@ dotenv.config();
 
 const pool = new Pool({
   user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
   database: process.env.PG_DB_NAME,
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
--- server/routes/stats.js
+++ server/routes/stats.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const pool = new Pool({
   user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
   database: process.env.PG_DB_NAME,
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
